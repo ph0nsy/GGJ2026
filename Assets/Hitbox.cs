3b3de07fc894c2112622e41e24a0e595083361e9
ViskoCode <marianodlt99@gmail.com>
2026-01-31 00:02:28 +0100

    WIP

--- Assets/Hitbox.cs
+++ Assets/Hitbox.cs
@@ -10,12 +10,12 @@ public class Hitbox : MonoBehaviour
     float angle;
     float width;
 
-    private Collider2D collider;
+    //private Collider2D collider;
 
     // Start is called before the first frame update
     void Start()
     {
-        collider = GetComponent<Collider2D>();
+        //collider = GetComponent<Collider2D>();
         //if (collider is CircleCollider2D) {
         //    collider?.radius = RANGE;
         //}
@@ -39,29 +39,32 @@ public class Hitbox : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (GetComponent<Collider2D>() is CircleCollider2D) {
-            checkArchHitBox(other.gameObject.getComponent.offset, mouseDirection, range, angle);
+            checkArchHitBox(other.transform.position, mouseDirection, range, angle);
         }
-        else if (GetComponent<Collider2D>() is SquareCollider2D){
+        else if (GetComponent<Collider2D>() is BoxCollider2D){
             /// YOU ARE ALREADY INSIDE IDIOT
         }
     }
 
 
-    bool checkArchHitBox(Vector3 otherPosition, Vector3 direction; float radius, float angle){
-        Vector3 relativeOtherPos = otherPosition - GetComponent<Collider2D>().offset;
+    bool checkArchHitBox(Vector3 otherPosition, Vector3 direction, float radius, float angle)
+    {   
+        // NO USAR OFFSET O ME CORTO LA POLLA
+        Vector3 relativeOtherPos = otherPosition - GetComponent<Collider2D>().transform.position;
 
         Vector2 polarRelOther = getPolarCoordinates(relativeOtherPos);
     
         Vector2 polarDirection = getPolarCoordinates(direction.normalized);
-        Vector2 Aa = new Vector2(radius, polarDirection+(angle/2));
-        Vector2 Bb = new Vector2(0, polarDirection-(angle/2));
+        Vector2 Aa = new Vector2(radius, polarDirection.y+(angle/2));
+        Vector2 Bb = new Vector2(0, polarDirection.y-(angle/2));
 
         return checkInsideBox(Aa, Bb, polarRelOther);
     }
 
 
 
-    Vector2 getPolarCoordinates(Vector3 euclidCoord){
+    Vector2 getPolarCoordinates(Vector3 euclidCoord)
+    {
         float x = euclidCoord.x;
         float y = euclidCoord.y;
         float angle = Mathf.PI/2;
@@ -80,8 +83,7 @@ public class Hitbox : MonoBehaviour
     }
 
     bool checkInsideBox(Vector2 Aa, Vector2 Bb, Vector2 position){
-        return (position.x < Aa.x && position.x > Bb.x) && 
-        (position.y < Aa.y && position.y > Bb.y);
+        return (position.x < Aa.x && position.x > Bb.x) && (position.y < Aa.y && position.y > Bb.y);
     }
 
     Vector3 getMouseDirection(){
@@ -93,7 +95,9 @@ public class Hitbox : MonoBehaviour
     }
 
     void setMouseDirectionWithPositions(Vector3 mousePosition){
-        mouseDirection = mousePosition - GetComponent<Collider2D>().offset;
+                // NO USAR OFFSET O ME CORTO LA POLLA
+
+        mouseDirection = mousePosition - GetComponent<Collider2D>().transform.position;
         mouseDirection = mouseDirection.normalized; 
     }
 
