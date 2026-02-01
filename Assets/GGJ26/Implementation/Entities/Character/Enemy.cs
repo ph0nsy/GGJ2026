1b71ffa20d9ff5ed1662782bbc53d7b9a3fb158c
ViskoCode <marianodlt99@gmail.com>
2026-02-01 04:13:58 +0100

    Behaviour changes

--- Assets/GGJ26/Implementation/Entities/Character/Enemy.cs
+++ Assets/GGJ26/Implementation/Entities/Character/Enemy.cs
@@ -21,6 +21,37 @@ public class Enemy : Character
     {
         m_fsm.Init(States, transform);
         m_perception.Range = ((EnemyData)Data).awarenessRange;
+        m_health.OnDeath += updateAltarCount;
+    }
+
+    Altar getAltarOfSameType()
+    {
+        foreach (Altar altar in FindObjectsOfType<Altar>())
+        {
+            if (altar.Type == Type)
+            {
+                return altar;
+            }
+        }
+        return null;
+    }
+
+    void updateAltarCount()
+    {
+        Altar altar= getAltarOfSameType();
+
+        altar?.altarProgress.souls++;
+        
+        if(altar?.altarProgress.souls == altar?.altarProgress.threshold){
+            foreach (Altar a in FindObjectsOfType<Altar>())
+            {
+                if (a.Type != Type && !Player.Instance.playerProgress.maskUnlocked.Contains(a.PairTypeToAttack()))
+                {
+                    a.altarProgress.threshold*=2;
+                }
+            }
+        }
+
     }
 
     void FixedUpdate()
---
+++ Assets/GGJ26/Implementation/Entities/Data/EEntityType.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EEntityType
+{
+    Bot,
+    Lion,
+    Fiddle,
+    Player
+}
